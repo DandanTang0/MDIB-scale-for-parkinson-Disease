a8c166ba5ae55af870e07c62461516bbaa6a66cc
Dandan Tang <150409008+DandanTang0@users.noreply.github.com>
2026-06-20 13:19:03 -0400

    Update 1c_set_officer_properties.R

--- code/1c_set_officer_properties.R
+++ code/1c_set_officer_properties.R
@@ -1,18 +1,38 @@
 # ---------------------------------------------------------------------------- #
-# Set "officer" Package Properties
-# Original code by: Jeremy W. Eberle
-# Minor modifications by: Dandan Tang
-# ---------------------------------------------------------------------------- #
 
-# Set section and text properties that can be sourced in other scripts
+# Define project-wide section and text-formatting objects used when exporting
+# Word documents with the officer package. These objects are sourced by scripts
+# that create manuscript tables or supplemental documents.
 
 library(officer)
 
-psect_prop <- prop_section(page_size(orient = "portrait", width = 8.5, height = 11),
-                           type = "nextPage")
-lsect_prop <- prop_section(page_size(orient = "landscape", width = 8.5, height = 11),
-                           type = "nextPage")
+psect_prop <- officer::prop_section(
+  page_size = officer::page_size(orient = "portrait", width = 8.5, height = 11),
+  type = "nextPage"
+)
 
-text_prop <- fp_text_lite(color = "black", font.size = 12, font.family = "Times New Roman")
-text_prop_bold   <- update(text_prop, bold = TRUE)
-text_prop_italic <- update(text_prop, italic = TRUE)
+lsect_prop <- officer::prop_section(
+  page_size = officer::page_size(orient = "landscape", width = 8.5, height = 11),
+  type = "nextPage"
+)
+
+text_prop <- officer::fp_text_lite(
+  color = "black",
+  font.size = 12,
+  font.family = "Times New Roman"
+)
+
+text_prop_bold <- officer::fp_text_lite(
+  color = "black",
+  font.size = 12,
+  font.family = "Times New Roman",
+  bold = TRUE
+)
+
+text_prop_italic <- officer::fp_text_lite(
+  color = "black",
+  font.size = 12,
+  font.family = "Times New Roman",
+  italic = TRUE
+)
+
